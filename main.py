38cdd5cc80839f15b88ad07abba0869a7ae8811c
alexlomu <91721507+alexlomu@users.noreply.github.com>
2023-05-14 19:15:27 +0200

    multiprocessing

--- main.py
+++ main.py
@@ -2,27 +2,39 @@ from multiprocessing import Pool
 import time
 import random
 
-
+urls = ["a.com", "b.com", "c.com", "d.com"]
 def scrape(url):
     print("starting", url)
     duration = round(random.random(),3)
     time.sleep(duration)
     print("finished", url, "time taken:", duration, "seconds")
     return url, duration
-urls = ["a.com", "b.com", "c.com", "d.com"]
+
 
 #Secuencial
 def secuencial():
-    time = 0
+    time_sec = 0
     for url in urls:
-        time += scrape(url)[1]
-    print("El tiempo total ha sido:",time,"segundos")
-secuencial()
+        time_sec += scrape(url)[1]
+    print("El tiempo total ha sido:",time_sec,"segundos")
+
 #Multiproceamiento
 def multiprocesamiento():
     initial_time = time.time()
     pool = Pool(processes=4)
     data = pool.map(scrape, urls)
     pool.close()
+    pool.join()
+    for row in data:
+        print(row)
+    time_mult = time.time() - initial_time
+    print("El tiempo total ha sido:",time_mult,"segundos")
+
+
+multiprocesamiento()
+    
+
+
+
 
     
